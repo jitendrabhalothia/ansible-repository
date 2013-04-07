f2c01dfcecc5c7d518f45fab7b07ead5a64e7a35
Some User <someuser@domain.com>
2013-04-07 17:59:04 +0000

    first version of functionnal graphite install

--- graphite/templates/run-web.sh
+++
@@ -1,5 +0,0 @@
-# {{ ansible_managed }}
-
-# @see http://uwsgi-docs.readthedocs.org/en/latest/WSGIquickstart.html
-# TODO use a ini config file to setup uwsgi options and then ini graphite with bin/uwsgi config_file.ini
-{{ graphite_venv_path }}/bin/uwsgi --http :{{ graphite_uwsgi_port|default('8085') --master --processes 4 --home {{ graphite_venv_path }} --pythonpath {{ graphite_venv_path }}/webapp/graphite --wsgi-file {{ graphite_venv_path }}/conf/graphite.wsgi
